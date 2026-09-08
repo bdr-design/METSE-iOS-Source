5e6220a44d68db22dca954168f8a0217c4076233
bdr-design <bdr.600.aa@gmail.com>
2026-09-08 16:54:46 +0300

    materials: define bounded penetration and ricochet contract

--- Engine/Core/METSEMaterialCore.hpp
+++ Engine/Core/METSEMaterialCore.hpp
@@ -9,7 +9,12 @@ struct MaterialBallisticProfile {
     double penetrationThresholdJoules = 0.0;
     double retainedEnergyFraction = 0.0;
     double retainedVelocityFraction = 0.0;
+    double maxPenetrationThicknessMeters = 0.0;
+    double ricochetMinEnergyJoules = 0.0;
+    double ricochetMaxNormalCosine = 0.0; // lower cosine = more glancing impact required
+    double ricochetRetainedVelocityFraction = 0.0;
     bool penetrable = false;
+    bool ricochetEligible = false;
 };
 
 class MaterialCore final {
